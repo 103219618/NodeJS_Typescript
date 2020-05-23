ceee9f28f46e7c3e10b56748e13974c3b6a6f35d
Syed Razvi <103219618@swin.edu.au>
2020-05-24 03:10:38 +1000

    typescript

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,51 @@
-let myButton = document.getElementById("myButton") as HTMLButtonElement | null;
-if (myButton == null) {
+
+let addButton = document.getElementById("addButton") as HTMLButtonElement | null;
+let elements = document.getElementById("elements") as HTMLElement;
+let field = document.getElementById("field") as HTMLDataElement;
+let clrButton = document.getElementById("clrButton");
+
+if (addButton == null) {
     alert("Button not found.");
 }
-
 else {
-    
-    myButton.onclick = function () {
-        if(myButton == null) return;
-        alert(myButton.value);
-    }
 
+    addButton.onclick = function () {
+        let myNewElement = document.createElement("div");
+        let myNewElementText = document.createTextNode(field.value);
+        myNewElement.appendChild(myNewElementText);
+        elements.appendChild(myNewElement);
+        field.value = ""
+        myNewElement.style.backgroundColor = "#f7f5f5";
+        myNewElement.style.fontSize = "20px";
+        myNewElement.style.marginBottom = "10px";
+        myNewElement.style.borderRadius = "5px";
+        myNewElement.style.height = "40px, auto";
+        myNewElement.style.padding = "20px 20px 20px 20px";
+
+        myNewElement.onmouseover = function () {
+            myNewElement.style.backgroundColor = "darkgrey";
+            myNewElement.style.fontSize = "21px";
+        }
+
+        myNewElement.onmouseout = function () {
+            myNewElement.style.backgroundColor = "#f7f5f5";
+            myNewElement.style.fontSize = "20px";
+        }
+
+        myNewElement.onclick = function () {
+            elements.style.textDecoration = "line-through";
+        }
+
+        if (clrButton == null) {
+            alert("Button not found.");
+        }
+        else {
+            clrButton.onclick = function () {
+                elements.innerHTML = "";
+            }
+        }
+
+
+
+    }
 }
